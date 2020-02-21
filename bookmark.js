9784928de63d8fe515c873739d03473c3a9e6fb4
Brad Laney <brad.j.laney@gmail.com>
2020-02-21 13:10:01 -0600

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -1,1 +1,1 @@
-javascript:var q=(n)=>{n.setAttribute('style', 'position: static !important; overflow: visible !important;');for (var c of n.children){q(c);}};q(document.body);
+javascript:var q=(n)=>{n.setAttribute('style', 'position:static !important;overflow:visible !important;width:auto !important;height:auto !important;');for (var c of n.children){q(c);}};q(document.body);
